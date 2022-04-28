d7fdac4f5dfc956492abf8f2d5256f455a29cc64
Jasmin Bouasavatdy <jasminbouasavatdy@hotmail.com>
2022-04-28 09:55:54 -0400

    fixed deployment

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,6 +18,7 @@ const styles = {
   }
 };
 
+
 export default function Footer() {
   return (
     <>
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -21,7 +21,10 @@ const styles = {
   },
   projectContainer: {
     height: '350px',
-    overflow: 'scroll'
+    overflow: 'scroll',
+  },
+  p: {
+    color: '#3f51b5',
   }
 };
 export default function Portfolio() {
@@ -29,25 +32,25 @@ export default function Portfolio() {
     <div className="text-center py-4"style={styles.card}>
       <h1 style={styles.heading}>Portfolio</h1>
 
-        <p>Chiron Web Application</p>
+        <p style={styles.p}>Chiron Web Application</p>
       <div style={styles.projectContainer} className='projectContainer'>
       <a href="https://polar-peak-28753.herokuapp.com/">
         <img class="img-fluid" src={Chiron} alt="Chiron Site"></img>
         </a>
       </div>
-      <p>Binge-Watching Web Application</p>
+      <p style={styles.p}>Binge-Watching Web Application</p>
       <div style={styles.projectContainer} className='projectContainer'>
       <a href="https://jasminbouasavatdy.github.io/binge-watching/">
         <img class="img-fluid" src={Bw} alt="Binge-Watching Site"></img>
         </a>
       </div>
-      <p>Moon Web Application</p>
+      <p style={styles.p}>Moon Web Application</p>
       <div style={styles.projectContainer} className='projectContainer'>
         <a href='https://jasminbouasavatdy.github.io/mini_project_1/'>
         <img class="img-fluid" src={Moon} alt="Moon Site"></img>
         </a>
       </div>
-      <p>CSS Snippet Project</p>
+      <p style={styles.p}>CSS Snippet Project</p>
       <div style={styles.projectContainer} className='projectContainer'>
         <a href="https://jasminbouasavatdy.github.io/css-snippet/">
         <img class="img-fluid" src={Css} alt="CSS Snippet "></img>
